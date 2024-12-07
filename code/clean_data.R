630a01276d944f94dd0ca52c8603322438903f76
Anna <anna.schmidt@uvm.edu>
2024-12-07 10:54:32 -0500

    initial commit to set up project

---
+++ code/clean_data.R
@@ -0,0 +1,6 @@
+# Data cleaning
+# AGS
+# December 7, 2024
+
+# Read in "profiler_cleaned_final" RDS object that was generated after some cleaning steps in my "SPF_profiler" R project
+profiler_data <- readRDS(file = "data/profiler_cleaned_final.rds")
